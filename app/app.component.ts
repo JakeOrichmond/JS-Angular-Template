4d7c2be260fbf92df93d0d5d1073efb5836e86a6
Jake Richmond <jake@Jakes-iMac.home>
2016-06-02 13:48:46 -0700

    Angular Template text change

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from 'angular2/core';
 @Component({
   selector: 'my-app',
   template:`<div class="container">
-              <h1>Skeleton Angular2 App!</h1>
+              <h1>Skeleton Angular2 Template!</h1>
             </div>`
 })
 export class AppComponent {
